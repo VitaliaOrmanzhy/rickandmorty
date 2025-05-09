feffe233defac72e72cb5f34bca72680662175c7
VitaliaOrmanzhy <vormanzi812@gmail.com>
2025-05-09 14:54:44 +0300

    removed unused type

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CharacterType, ListType, ProfileType } from "../types/types";
+import { ListType, ProfileType } from "../types/types";
 
 const instance = axios.create({
   baseURL: "https://rickandmortyapi.com/api/",
